4b282984347fd614984a91c9b9d2c4fbc83e4249
pinusc <giuseppe.stelluto@gmail.com>
2020-11-07 16:21:35 -0500

    dynamic map markers

--- vue/src/components/Map.vue
+++ vue/src/components/Map.vue
@@ -4,7 +4,8 @@
 
 <template>
   <div id="mapview">
-    <h1>map</h1>
+  <button v-on:click="initmap"> init </button>
+  <h1 v-on:click="ajaxm">map</h1>
     <p>{{ msg }}</p>
     <div id="mapid"></div>
   </div>
@@ -14,7 +15,34 @@
 export default {
   name: 'Map',
   props: {
-    msg: String
+    msg: String,
+  },
+  methods:{
+    initmap () { 
+        // var mymap = L.map('mapid').setView([51.505, -0.09], 13)
+        var mymap = this.$L.map('mapid').setView([51.500944, 0.124618], 13);
+        this.$L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoicGludXNjIiwiYSI6ImNraDgyNjRrejA1ZGEycnFpZXU5dTJqMjkifQ.RyyPpqxIelqUtOiWdn4efg'
+        }).addTo(mymap);
+        this.mymap = mymap;
+    },
+    async ajaxm () {
+      const { data } = await this.$http.get(
+              'http://localhost:5000/api/otm?lat=51.500944&lon=0.124618', 
+      );
+      for (var i = 0; i < data.length; i++) {
+        var point = [data[i].point.lat, data[i].point.lon]
+        console.log(point)
+        this.$L.marker(point).addTo(this.mymap);
+      }
+      console.log(data);
+      // example response: { id: 1, name: "something" }
+    }
   }
 }
 </script>
