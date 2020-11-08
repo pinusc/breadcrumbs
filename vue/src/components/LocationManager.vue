d3738d062509e8ec47d3e7df3d96724b8a250595
pinusc <giuseppe.stelluto@gmail.com>
2020-11-07 21:26:04 -0500

    startpage

--- vue/src/components/LocationManager.vue
+++ vue/src/components/LocationManager.vue
@@ -15,42 +15,42 @@ function displayLocation(position) {
 
 function displayError(error) { 
 
-//get a reference to the HTML element for writing result
-var locationElement = document.getElementById("locationData");
+    //get a reference to the HTML element for writing result
+    var locationElement = document.getElementById("locationData");
 
-//find out which error we have, output message accordingly
-switch(error.code) {
-case error.PERMISSION_DENIED:
-	locationElement.innerHTML = "Permission was denied";
-	break;
-case error.POSITION_UNAVAILABLE:
-	locationElement.innerHTML = "Location data not available";
-	break;
-case error.TIMEOUT:
-	locationElement.innerHTML = "Location request timeout";
-	break;
-case error.UNKNOWN_ERROR:
-	locationElement.innerHTML = "An unspecified error occurred";
-	break;
-default:
-	locationElement.innerHTML = "Who knows what happened...";
-	break;
-}}
+    //find out which error we have, output message accordingly
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            locationElement.innerHTML = "Permission was denied";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            locationElement.innerHTML = "Location data not available";
+            break;
+        case error.TIMEOUT:
+            locationElement.innerHTML = "Location request timeout";
+            break;
+        case error.UNKNOWN_ERROR:
+            locationElement.innerHTML = "An unspecified error occurred";
+            break;
+        default:
+            locationElement.innerHTML = "Who knows what happened...";
+            break;
+    }}
 
 export default {
-  name: 'LocationManager',
-  props: {
-  },
-  methods:{
-    queryLocation () { 
-        //check if the geolocation object is supported, if so get position
-        if (navigator.geolocation)
-            navigator.geolocation.getCurrentPosition(displayLocation, displayError);
-        else
-            document.getElementById("locationData").innerHTML = "Sorry - your browser doesn't support geolocation!";
+    name: 'LocationManager',
+    props: {
     },
-    
-  }
+    methods:{
+        queryLocation () { 
+            //check if the geolocation object is supported, if so get position
+            if (navigator.geolocation)
+                navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+            else
+                document.getElementById("locationData").innerHTML = "Sorry - your browser doesn't support geolocation!";
+        },
+
+    }
 }
 </script>
 
--- vue/src/views/Start.vue
+++ vue/src/views/Start.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="home">
+      <section class="hero is-primary is-bold">
+          <div class="hero-body">
+              <h1 class=title>Breadcrumbs</h1>
+          </div>
+      </section>
+      <section>
+          <div class="box has-background-info">
+              <h1 class="subtitle has-text-light">What kind of places would you like to see?</h1>
+              <ul id=categories-list>
+                  <li v-for="i in categories.length" :key=i>
+                      <div class="box" >
+                          <label class="checkbox">
+                              <span class="checkbox__input">
+                                  <input type="checkbox" name="checked" checked v-model="categoriesChecked[i-1]">
+                                  <span class="checkbox__control">
+                                      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' aria-hidden="true" focusable="false">
+                                          <path fill='none' stroke='currentColor' stroke-width='3' d='M1.73 12.91l6.37 6.37L22.79 4.59' /></svg>
+                                  </span>
+                              </span>
+                              <span v-bind:class="{ 'has-text-weight-bold': categoriesChecked[i-1] }" class="radio__label">{{ categoryNames[i-1] }}</span>
+                          </label>
+                      </div>
+                  </li>
+              </ul>
+          </div>
+          <div class="card has-background-warning">
+              <div class="card-content">
+                  {{ categoriesChecked }}
+              </div>
+          </div>
+      </section>
       <router-link to="/SelectDestination">Choose a destination</router-link>
   </div>
 </template>
@@ -8,6 +39,67 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Start',
+    name: 'Start',
+    data: function() {
+        return {
+            categories: ["architecture", "cultural", "historic", "industrial_facilities", "natural", "religion", "other"],
+            categoriesChecked: [false, false, false, false, false, false],
+            categoryNames: ["Architecture", "Cultural", "Historic", "Industrial facilities", "Natural", "Religion", "Other"]
+        }
+    }
 }
 </script>
+
+<style scoped lang=scss>
+.card {
+    margin: 1em;
+}
+
+#categories-list .box {
+    margin: 0.5em;
+}
+
+.checkbox {
+    display: grid;
+    grid-template-columns: min-content auto;
+    grid-gap: 0.5em;
+    font-size: 1.5rem;
+    color: var(--color);
+}
+
+.checkbox__input {
+  input {
+    opacity: 0;
+    width: 1em;
+    height: 1em;
+  }
+}
+
+.checkbox__control {
+  display: inline-grid;
+  width: 1em;
+  height: 1em;
+  border-radius: 0.25em;
+  border: 0.1em solid currentColor;
+}
+
+.checkbox__input {
+  display: grid;
+  grid-template-areas: "checkbox";
+  
+  > * {
+    grid-area: checkbox;
+  }
+}
+
+.checkbox__control svg {
+  transition: transform 0.1s ease-in 25ms;
+  transform: scale(0);
+  transform-origin: bottom left;
+}
+
+.checkbox__input input:checked
+ + .checkbox__control svg {
+  transform: scale(1);
+}
+</style>
