cc05521b8f9ddae156918fda168e1bccfe6ba595
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2020-11-08 02:14:04 -0500

    I'm here button on venue now actually takes you where you are supposed to go

--- vue/src/views/Venue.vue
+++ vue/src/views/Venue.vue
@@ -1,17 +1,52 @@
 <template>
   <div class="home">
-      <router-link to="/Final">I'm here {{xid}}</router-link>
+      <p>xid:  {{this.xid}}</p>
+      <h1>You're going to <b>{{this.info.name}}</b></h1>
+      <div id="wiki-blurb" v-if="hasWikipediaExtracts">
+          <h1>{{this.info.wikipedia_extracts.title.substr(3)}}</h1>
+          <div v-html="this.info.wikipedia_extracts.html"></div>
+      </div>
+      <div v-else>
+          <p>Unfortunately, we couldn't find more info on this venue. Try using a search engine?</p>
+      </div>
+      <button @click="finalizeVenue">I'm here</button>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import router from '@/router'
 
 export default {
   name: 'Venue',
   data: function(){
     return {
       xid:-1
+    }
+  },
+  computed:{
+    hasAdress: function(){
+          return 'address' in this.$root.$data.vuey.venue_detail[this.xid]
+      },
+      hasWikipediaLink: function(){
+          return 'wikipedia' in this.$root.$data.vuey.venue_detail[this.xid]
+      },
+      hasWikipediaExtracts: function(){
+          return 'wikipedia_extracts' in this.$root.$data.vuey.venue_detail[this.xid]
+      },
+      info: function(){
+          return this.$root.$data.vuey.venue_detail[this.xid];
+      }
+  },
+  methods:{
+    finalizeVenue: function(){
+      if (this.info.point.lat == this.$root.$data.vuey.finalDestinationLocation.lat &
+        this.info.point.lon == this.$root.$data.vuey.finalDestinationLocation.lon ){
+          router.push("Final");
+      }else{
+        router.push("ChooseNextVenue");
+      }
     }
   },
   created() {
